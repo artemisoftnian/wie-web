f60d26b23af5a2f9fd3bda5604576a05e6f7d35e
hackinc2000 <hackinc2000@gmail.com>
2021-02-01 12:58:39 -0400

    Correct Files

--- src/components/fontawsome/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-// import the library
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {} from '@fortawesome/free-solid-svg-icons';
-
-
-library.add();
